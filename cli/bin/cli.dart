b5df4cf99c04d9485f685ddefad7132eae190187
Haha <duytontran@hotmail.com>
2026-03-23 17:03:23 -0400

    added http API

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -8,27 +8,36 @@ void main(List<String> arguments) {
     print('Hello, Dart!');
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version: $version');
-  } else if (arguments.first == 'search') {
+  } else if (arguments.first == 'wikipedia') {
+    // Pass all arguments *after* 'wikipedia' to searchWikipedia
     final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
-    searchWikipedia(inputArgs);
+    searchWikipedia(inputArgs); // Call searchWikipedia (no 'await' needed here for main)
   } else {
-    printUsage();
+    printUsage(); // Catch all for any unrecognized command.
   }
 }
 
-void searchWikipedia(List<String>? arguments) {
+void searchWikipedia(List<String>? arguments) async{
   final String articleTitle;
 
   // If user didn't pass in arguments, request an article title
   if (arguments == null || arguments.isEmpty) {
     print('Please provide an article title.');
-    articleTitle = stdin.readLineSync() ?? '';
+    final inputFromStdin = stdin.readLineSync(); // Read input
+
+    if(inputFromStdin == null || inputFromStdin.isEmpty){
+      print('No article title provided. Existing.');
+      return;
+    }
+    articleTitle = inputFromStdin;
   } else {
     articleTitle = arguments.join(' ');
   }
   print('Looking up articles about "$articleTitle". Please wait.');
-  print('Here ya go!');
-  print('(Pretend this is an article about "$articleTitle")');
+  
+  // Call the API and await the result
+  var articleContent = await getWikipediaArticle(articleTitle);
+  print(articleContent); // Print the full article response (raw JSON for now)
 }
 
 void printUsage() {
